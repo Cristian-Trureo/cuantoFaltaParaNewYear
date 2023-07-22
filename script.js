e6d0d284f400b5a2255cb9ecb80699b8c32724f6
Cristian.Trureo <c.trureo87@gmail.com>
2023-07-22 12:41:59 -0400

    eliminando función extra

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 var parrafo = document.getElementById('today');
 var parrafoF = document.getElementById('faltan');
-var parrafoS = document.getElementById('falta18');
 
 function mostrarFecha() {
   var ahora = new Date();
@@ -9,16 +8,7 @@ function mostrarFecha() {
   parrafo.innerHTML = fechaActual;
 }
 
-function mostrarFaltante18(){
-  var ahora = new Date();
-  var proximo18 = new Date("September 18, 2023 00:00:00");
-  var diferenciaMSS = proximo18.getTime() - ahora.getTime();
-  var segundosS = Math.floor(diferenciaMSS / 1000);
-  var minutosS = Math.floor(segundosS / 60);
-  var horasS = Math.floor(minutosS / 60);
-  var diasS = Math.floor(horasS / 24);
-  parrafoS.innerHTML = `${diasS} días ${horasS % 24} horas ${minutosS % 60} minutos ${segundosS % 60} segundos`;
-}
+
 
 function mostrarFechaF() {
   var ahora = new Date();
@@ -31,12 +21,11 @@ function mostrarFechaF() {
   parrafoF.innerHTML = `${diasF} días ${horasF % 24} horas ${minutosF % 60} minutos ${segundosF % 60} segundos`;
 }
 
-mostrarFecha();
-mostrarFaltante18()
-mostrarFechaF();
 
 
 setInterval(mostrarFecha, 1000);
-setInterval(mostrarFaltante18, 1000);
+setInterval(function() {
+  mostrarFaltante18("January", 2, 2024);
+}, 1000);
 
 setInterval(mostrarFechaF, 1000);
